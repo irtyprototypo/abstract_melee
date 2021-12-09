8272accc627dac1933dc0eb1a911eaf043214ee4
irtypo <irtypo@gmail.com>
2021-12-09 06:57:54 -0700

    svg refactoring

--- js/index.js
+++ js/index.js
@@ -40,6 +40,10 @@ let gameOver = false;
  * - action state special move quirk
 **/
 
+
+
+
+
 function init(){
     GAME_FRAMES = slp_replay.data.frames;
     GAME_METADATA = slp_replay.data.metadata;
@@ -54,6 +58,7 @@ function init(){
     // console.log(GAME_DATA);
 
     ctx = createCanvas();
+    drawModel();
 
     setPlayers();
     setPerspective(0);
@@ -66,13 +71,12 @@ function init(){
 
     createZones(stage.name);
     // generateInflectionPoints('grabs rolls');
-    specifyInflectionPoint();
+    // specifyInflectionPoint();
 
 
 
     requestAnimationFrame(mainLoop);
 }
-
 
 function mainLoop(){
     // let frameRate = 16.67 * 2;
@@ -102,13 +106,16 @@ function mainLoop(){
     setTimeout(_=> { requestAnimationFrame(mainLoop); }, frameRate);
 }
 
+
+
+
+/****** decision logic ******/
 function generateOptions(){
 
     // what is an option a function of?
     // option(perspective_position, opponent_position, history, percetns, etc...)
 
 }
-
 
 function determinePhase(){
 
@@ -175,7 +182,6 @@ function determinePhase(){
         $("#state_an_g, #state_dn_g").children(0).attr('fill', '#fff');
     }
 }
-
 
 function determineState(){
     perspective.state = 'default';
@@ -246,7 +252,58 @@ function determineState(){
 
 }
 
+function generateInflectionPoints(options){
+    let lookingFor = [];
+    let knockdowns = [183, 191];
+    let techOptions = [199, 200, 201, 202, 203, 204];
+    let grabs = [213, 226];
+    let shield = [179, 180, 181, 182];
+    let rolls = [233, 234];
+    // catching double jump
+    // the rest of the model states
+    
 
+    if(options.includes('knockdown'))  lookingFor = lookingFor.concat(knockdowns);
+    if(options.includes('techchase'))  lookingFor = lookingFor.concat(techOptions);
+    if(options.includes('grab'))        lookingFor = lookingFor.concat(grabs);
+    if(options.includes('shield'))      lookingFor = lookingFor.concat(shield);
+    if(options.includes('roll'))        lookingFor = lookingFor.concat(rolls);
+    if(options.length == 0) lookingFor = [];
+
+    for(let i=0; i < lastFrame; i++){
+        let prevAStateId = GAME_FRAMES[i-1].players[perspective.index].pre.actionStateId;
+        let aStateId = GAME_FRAMES[i].players[perspective.index].pre.actionStateId;
+        // let nextAStateId = GAME_FRAMES[i+1].players[perspective.index].pre.actionStateId;
+            
+        if (lookingFor.includes(aStateId) && !lookingFor.includes(prevAStateId) )
+            perspective.inflectionPoints.push(i)
+        
+    }
+    if(lookingFor.length == 0)  perspective.inflectionPoints = [];
+    
+    perspective.inflectionPointsReversed = perspective.inflectionPoints.slice().reverse();
+    
+   console.log(`Inflection points loaded: ${options}`);
+}
+
+function zonesOccupied(){
+    playerList.forEach((player, x) =>{
+        player.zones = new Set();
+        zoneList.forEach((zone, y) =>{
+            if(zone.isInside(player.positionX, player.positionY))
+                player.zones.add(zone);
+            if(zone.name == 'Below')
+                if(zone.isBelow(player.positionX, player.positionY))
+                    player.zones.add(zone);
+        });
+    });
+}
+
+
+
+
+
+/****** graphics logic ******/
 function setPerspective(playerIndex){
     playerList.forEach(p =>{
         if (p.index === playerIndex){
@@ -261,7 +318,6 @@ function setPerspective(playerIndex){
     });
     console.log(`Perspective set to ${perspective.name}.`);
 }
-
 
 function createZones(name){
     let edgeZoneHeight = 30;
@@ -344,40 +400,6 @@ function createZones(name){
                                     
 }
 
-function generateInflectionPoints(options){
-    let lookingFor = [];
-    let knockdowns = [183, 191];
-    let techOptions = [199, 200, 201, 202, 203, 204];
-    let grabs = [213, 226];
-    let shield = [179, 180, 181, 182];
-    let rolls = [233, 234];
-    // catching double jump
-    // the rest of the model states
-    
-
-    if(options.includes('knockdown'))  lookingFor = lookingFor.concat(knockdowns);
-    if(options.includes('techchase'))  lookingFor = lookingFor.concat(techOptions);
-    if(options.includes('grab'))        lookingFor = lookingFor.concat(grabs);
-    if(options.includes('shield'))      lookingFor = lookingFor.concat(shield);
-    if(options.includes('roll'))        lookingFor = lookingFor.concat(rolls);
-    if(options.length == 0) lookingFor = [];
-
-    for(let i=0; i < lastFrame; i++){
-        let prevAStateId = GAME_FRAMES[i-1].players[perspective.index].pre.actionStateId;
-        let aStateId = GAME_FRAMES[i].players[perspective.index].pre.actionStateId;
-        // let nextAStateId = GAME_FRAMES[i+1].players[perspective.index].pre.actionStateId;
-            
-        if (lookingFor.includes(aStateId) && !lookingFor.includes(prevAStateId) )
-            perspective.inflectionPoints.push(i)
-        
-    }
-    if(lookingFor.length == 0)  perspective.inflectionPoints = [];
-    
-    perspective.inflectionPointsReversed = perspective.inflectionPoints.slice().reverse();
-    
-   console.log(`Inflection points loaded: ${options}`);
-}
-
 function displayMediaButtons(){
     if (currentFrame < lastFrame){
         document.getElementById('previous-inflection-point-btn').disabled = false;
@@ -394,7 +416,6 @@ function displayMediaButtons(){
         document.getElementById('previous-frame-btn').disabled = true;
     }
 }
-
 
 function specifyInflectionPoint(){
 
@@ -422,101 +443,6 @@ function specifyInflectionPoint(){
 
 }
 
-function drawGameScreen(){
-    // draw background
-    if(!trails){
-        cleanSlate();
-        stage.draw();
-    }
-    
-    drawHUD();
-    drawInflectionPoints();
-    drawPlaybackPosition();
-
-    if(zonesVisible > 0)    drawZones();
-    if(debugView)           drawDebugView();
-    // if(debugView)   drawMeleeGrid();
-
-    
-    // draw players
-    playerList.forEach( player => { player.draw(WINDOW_SCALER); });
-
-    drawProjectiles();
-}
-
-
-function drawProjectiles(){
-    if(!GAME_FRAMES[currentFrame].items)
-        return;
-    
-    GAME_FRAMES[currentFrame].items.forEach(item =>{
-        // console.log(`${playerList[item.owner].name} threw an item with state ${item.state} on frame ${currentFrame}.`);
-        // console.log(item.owner, item.state, item.typeId, currentFrame);
-        drawCircle(meleeToCanvasX(item.positionX), meleeToCanvasY(item.positionY), 3, playerList[item.owner].portColor, '#fff');
-    });
-}
-
-
-function drawHUD(){
-    let p1Percent = Math.round(GAME_FRAMES[currentFrame].players[0].pre.percent);
-    let p2Percent = Math.round(GAME_FRAMES[currentFrame].players[1].pre.percent);
-
-
-    ctx.font = "45px Arial";
-    
-    ctx.strokeStyle = playerList[0].portColor;
-    ctx.strokeText(`${p1Percent}%`, CANVAS_WIDTH * .31, CANVAS_HEIGHT * .92);
-    
-    ctx.strokeStyle = playerList[1].portColor;
-    ctx.strokeText(`${p2Percent}%`, CANVAS_WIDTH * .64, CANVAS_HEIGHT * .92);
-    
-    
-    
-    
-    
-    ctx.fillStyle = "#fff";
-    ctx.font = "20px Arial";
-    ctx.fillText(`${playerList[0].actionStateName}`, (CANVAS_WIDTH * .31) - ctx.measureText(playerList[0].actionStateName).width * .35, CANVAS_HEIGHT * .84);
-    ctx.fillText(`${playerList[1].actionStateName}`, (CANVAS_WIDTH * .64) - ctx.measureText(playerList[1].actionStateName).width * .35, CANVAS_HEIGHT * .84);
-    // ctx.stroke();
-    // ctx.closePath();
-}
-
-function drawZones(){
-    let intersect = new Set();
-
-    if(zonesVisible == 2){
-        zoneList.forEach(zone =>{ zone.draw('#fff'); })
-    } else {
-        playerList.forEach(player =>{
-            player.zones.forEach(zone =>{
-                zone.draw(player.portColor);
-            })
-        });
-
-        
-        for(let z of playerList[0].zones)
-            if(playerList[1].zones.has(z))
-                intersect.add(z);
-
-        intersect.forEach(zone => { zone.draw('#fff'); });
-    }
-}
-
-
-function zonesOccupied(){
-    playerList.forEach((player, x) =>{
-        player.zones = new Set();
-        zoneList.forEach((zone, y) =>{
-            if(zone.isInside(player.positionX, player.positionY))
-                player.zones.add(zone);
-            if(zone.name == 'Below')
-                if(zone.isBelow(player.positionX, player.positionY))
-                    player.zones.add(zone);
-        });
-    });
-}
-
 function setStage(){
     let selectedStage;
 
@@ -537,7 +463,6 @@ function setStage(){
     stage.setRightPlatform(selectedStage.rightPlatformLeft, selectedStage.rightPlatformRight, selectedStage.rightPlatformBottom);
     stage.setTopPlatform(selectedStage.topPlatformLeft, selectedStage.topPlatformRight, selectedStage.topPlatformBottom);
 }
-
 
 function updatePlayerPositions(){
     playerList.forEach((player, i, arr) =>{
@@ -564,64 +489,7 @@ function updatePlayerPositions(){
 }
 
 
-
-// aspect ratio convertors
-function meleeToCanvasX(meleeX){ return (CANVAS_WIDTH / 2 + stage.x_offset) + (meleeX * 73/60 * stage.x_scaler); }
-function meleeToCanvasY(meleeY){ return ((CANVAS_HEIGHT * stage.floor_offset) + (-meleeY * 73/60 * stage.y_scaler)); }
-function canvasToMeleeX(canvasX){ return ((CANVAS_WIDTH/2) - canvasX + stage.x_offset) * -60/73 / stage.x_scaler; }
-function canvasToMeleeY(canvasY){ return ((CANVAS_HEIGHT * stage.floor_offset - canvasY) * (60/73 / stage.y_scaler)); }
-
-
-function drawMeleeGrid(){
-    // vertical
-    let i = 7;
-    while(i < CANVAS_HEIGHT){
-        ctx.beginPath();
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = '#ff0';
-        ctx.moveTo(0, meleeToCanvasY(canvasToMeleeY(i)));
-        ctx.lineTo(CANVAS_WIDTH, meleeToCanvasY(canvasToMeleeY(i)));
-        ctx.stroke();
-        ctx.closePath();
-        i+= CANVAS_HEIGHT/10;
-    }
-
-    // horizontal
-    i = 10;
-    while(i < CANVAS_WIDTH){
-        ctx.beginPath();
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = '#ff0';
-        ctx.moveTo(meleeToCanvasX(canvasToMeleeX(i)), 0);
-        ctx.lineTo(meleeToCanvasX(canvasToMeleeX(i)), CANVAS_HEIGHT);
-        ctx.stroke();
-        ctx.closePath();
-        i+= CANVAS_WIDTH/10;
-    }
-}
-
-async function loadSlpDataFromServer(){
-
-    await fetch(`http://localhost:8080/slippi_stats`)
-    .then( res => { return res.json(); })
-    .then( stats => { GAME_STATS = stats; });
-
-    await fetch(`http://localhost:8080/slippi_settings`)
-    .then( res => { return res.json(); })
-    .then( settings => { GAME_SETTINGS = settings });
-    
-    await fetch(`http://localhost:8080/slippi_frames`)
-    .then( res => { return res.json(); })
-    .then( frames => { GAME_FRAMES = frames; });
-    
-    await fetch(`http://localhost:8080/slippi_metadata`)
-    .then( res => { return res.json(); })
-    .then( metadata => { GAME_METADATA = metadata });
-    
-}
-
-
-// toggle graphic buttons
+/****** toggle graphic buttons ******/
 function toggleRubberBand(){
     let btn = document.getElementById("char-band-btn");
     if(bandsVisible == true){
@@ -664,7 +532,6 @@ function toggleZoneVisibility(){
     console.log(`Zones ${visibility}.`)
 }
 
-
 function toggleStageFrame(){
     let btn = document.getElementById("stage-frame-btn");
     if(debugView == true){
@@ -681,8 +548,7 @@ function toggleStageFrame(){
 }
 
 
-
-// media controls
+/****** media controls ******/
 function mediaButtonPressed(action){
     let playBtn = document.getElementById('play-toggle-btn');
     switch(action){
@@ -741,32 +607,11 @@ function mediaButtonPressed(action){
 }
 
 
-// draw stuff
-function drawDot(e, canvas) {
-    let c = canvas.getContext('2d');
-    let mouseX = e.clientX;
-    let mouseY = e.clientY;
-    let mouseOffsetX = -10;
-    let mouseOffsetY = -10;
-    mouseX += mouseOffsetX;
-    mouseY += mouseOffsetY;
 
-    console.log(`c: (${mouseX}, ${mouseY})`);
-    console.log(`m: (${canvasToMeleeX(mouseX)}, ${canvasToMeleeY(mouseY)})`);
-    
-    c.fillStyle = "#543548";
-    c.fillRect (mouseX, mouseY, 5, 5);
-}
 
-function drawBox(){
-    ctx.beginPath();
-    ctx.rect(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2  + 50 , 150, 100);
-    ctx.lineWidth = "6";
-    ctx.strokeStyle = "red";
-    ctx.stroke();
-    ctx.closePath();
-}
 
+
+/****** draw stuff ******/
 function drawCircle(x, y, r, fillColor, strokeColor){
     ctx.beginPath();
     ctx.arc(x, y-r, r, 0, 2 * Math.PI);
@@ -875,7 +720,15 @@ function drawDebugView(){
     ctx.closePath();
 }
 
-function cleanSlate(){
+function createCanvas(){
+    let canvas = document.querySelector('#game-canvas');
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+    // canvas.addEventListener('click', event =>{ }, true);
+    return canvas.getContext('2d');
+}
+
+function clearCanvas(){
     ctx.beginPath();
     ctx.rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     // ctx.fillStyle = '#808080';
@@ -886,18 +739,135 @@ function cleanSlate(){
     ctx.closePath();
 }
 
-function createCanvas(){
-    let canvas = document.querySelector('#game-canvas');
-    canvas.width = CANVAS_WIDTH;
-    canvas.height = CANVAS_HEIGHT;
-    // canvas.addEventListener('click', event =>{ }, true);
-    return canvas.getContext('2d');
+function drawGameScreen(){
+    // draw background
+    if(!trails){
+        clearCanvas();
+        stage.draw();
+    }
+    
+    drawHUD();
+    drawInflectionPoints();
+    drawPlaybackPosition();
+
+    if(zonesVisible > 0)
+        drawZones();
+    if(debugView)
+        drawDebugView();
+        // drawMeleeGrid();
+
+    
+    // draw players
+    playerList.forEach( player => { player.draw(WINDOW_SCALER); });
+
+    drawProjectiles();
+}
+
+function drawProjectiles(){
+    if(!GAME_FRAMES[currentFrame].items)
+        return;
+    
+    GAME_FRAMES[currentFrame].items.forEach(item =>{
+        // console.log(`${playerList[item.owner].name} threw an item with state ${item.state} on frame ${currentFrame}.`);
+        // console.log(item.owner, item.state, item.typeId, currentFrame);
+        drawCircle(meleeToCanvasX(item.positionX), meleeToCanvasY(item.positionY), 3, playerList[item.owner].portColor, '#fff');
+    });
+}
+
+function drawHUD(){
+    let p1Percent = Math.round(GAME_FRAMES[currentFrame].players[0].pre.percent);
+    let p2Percent = Math.round(GAME_FRAMES[currentFrame].players[1].pre.percent);
+    let p1stocks = [];
+
+
+    ctx.font = "45px Arial";
+    
+    ctx.strokeStyle = playerList[0].portColor;
+    ctx.strokeText(`${p1Percent}%`, CANVAS_WIDTH * .31, CANVAS_HEIGHT * .92);
+    
+    ctx.strokeStyle = playerList[1].portColor;
+    ctx.strokeText(`${p2Percent}%`, CANVAS_WIDTH * .64, CANVAS_HEIGHT * .92);
+    
+    
+    ctx.fillStyle = "#fff";
+    ctx.font = "20px Arial";
+    ctx.fillText(`${playerList[0].actionStateName}`, (CANVAS_WIDTH * .31) - ctx.measureText(playerList[0].actionStateName).width * .35, CANVAS_HEIGHT * .84);
+    ctx.fillText(`${playerList[1].actionStateName}`, (CANVAS_WIDTH * .64) - ctx.measureText(playerList[1].actionStateName).width * .35, CANVAS_HEIGHT * .84);
+    // ctx.stroke();
+    // ctx.closePath();
+}
+
+function drawZones(){
+    let intersect = new Set();
+
+    if(zonesVisible == 2){
+        zoneList.forEach(zone =>{ zone.draw('#fff'); })
+    } else {
+        playerList.forEach(player =>{
+            player.zones.forEach(zone =>{
+                zone.draw(player.portColor);
+            })
+        });
+
+        
+        for(let z of playerList[0].zones)
+            if(playerList[1].zones.has(z))
+                intersect.add(z);
+
+        intersect.forEach(zone => { zone.draw('#fff'); });
+    }
+}
+
+function drawMeleeGrid(){
+    // vertical
+    let i = 7;
+    while(i < CANVAS_HEIGHT){
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#ff0';
+        ctx.moveTo(0, meleeToCanvasY(canvasToMeleeY(i)));
+        ctx.lineTo(CANVAS_WIDTH, meleeToCanvasY(canvasToMeleeY(i)));
+        ctx.stroke();
+        ctx.closePath();
+        i+= CANVAS_HEIGHT/10;
+    }
+
+    // horizontal
+    i = 10;
+    while(i < CANVAS_WIDTH){
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#ff0';
+        ctx.moveTo(meleeToCanvasX(canvasToMeleeX(i)), 0);
+        ctx.lineTo(meleeToCanvasX(canvasToMeleeX(i)), CANVAS_HEIGHT);
+        ctx.stroke();
+        ctx.closePath();
+        i+= CANVAS_WIDTH/10;
+    }
+}
+
+function drawModel(){
+    const svg = document.querySelector("svg");
+    const svgns = "http://www.w3.org/2000/svg";
+    let nBox = document.createElementNS(svgns, "rect");
+    
+    nBox.setAttribute("x", "10");
+    nBox.setAttribute("y", "10");
+    nBox.setAttribute("width", "20");
+    nBox.setAttribute("height", "20");
+    nBox.setAttribute("fill", "#5cceee");
+
+    svg.appendChild(nBox);
+    // console.log(svg);
 }
 
 
 
 
-// utils
+
+
+
+/****** utils ******/
 function guiNameColors(){
     for(let i=0; i < playerList.length; i++){
         document.getElementById(`p${i+1}-name`).innerHTML = playerList[i].name;
@@ -938,3 +908,29 @@ function setPlayers(){
     });
 
 }
+
+async function loadSlpDataFromServer(){
+
+    await fetch(`http://localhost:8080/slippi_stats`)
+    .then( res => { return res.json(); })
+    .then( stats => { GAME_STATS = stats; });
+
+    await fetch(`http://localhost:8080/slippi_settings`)
+    .then( res => { return res.json(); })
+    .then( settings => { GAME_SETTINGS = settings });
+    
+    await fetch(`http://localhost:8080/slippi_frames`)
+    .then( res => { return res.json(); })
+    .then( frames => { GAME_FRAMES = frames; });
+    
+    await fetch(`http://localhost:8080/slippi_metadata`)
+    .then( res => { return res.json(); })
+    .then( metadata => { GAME_METADATA = metadata });
+    
+}
+
+// aspect ratio convertors. there is probably a much better way to do this lmao
+function meleeToCanvasX(meleeX){ return (CANVAS_WIDTH / 2 + stage.x_offset) + (meleeX * 73/60 * stage.x_scaler); }
+function meleeToCanvasY(meleeY){ return ((CANVAS_HEIGHT * stage.floor_offset) + (-meleeY * 73/60 * stage.y_scaler)); }
+function canvasToMeleeX(canvasX){ return ((CANVAS_WIDTH/2) - canvasX + stage.x_offset) * -60/73 / stage.x_scaler; }
+function canvasToMeleeY(canvasY){ return ((CANVAS_HEIGHT * stage.floor_offset - canvasY) * (60/73 / stage.y_scaler)); }
